6298f53dd94a38fb8ce5a2f8aeb105be795218b7
Md Yeasin <mdyeasinhn@gmail.com>
2024-07-11 14:49:06 +0600

    get all food by spacic specific user api complete

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 5000
 const app = express();
 // middlewere
 const corsOptions = {
-    origin: ['http://localhost:5173/'],
-    Credential: true,
-    optionSuccessStatus: 200,
+  origin: ['http://localhost:5173/'],
+  Credential: true,
+  optionSuccessStatus: 200,
 
 }
 app.use(cors({ corsOptions }))
@@ -32,27 +32,41 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const itemsCollection = client.db('foodoko').collection('items')
+    const foodsCollection = client.db('foodoko').collection('foods')
     const ordersCollection = client.db('foodoko').collection("orders")
 
-       // Get all item data form db 
-       app.get('/items', async (req, res) => {
-        const result = await itemsCollection.find().toArray();
-        res.send(result);
+    // Get all item data form db 
+    app.get('/items', async (req, res) => {
+      const result = await itemsCollection.find().toArray();
+      res.send(result);
     })
     // Get single item data from db
-    app.get('/item/:id',async(req, res) =>{
-      const  id = req.params.id
-      const query = { _id : new ObjectId(id)}
+    app.get('/item/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
       const result = await itemsCollection.findOne(query)
       res.send(result)
     })
     // Save a order data in db
-    app.post('/order', async(req, res)=>{
+    app.post('/order', async (req, res) => {
       const orderData = req.body;
       const result = await ordersCollection.insertOne(orderData);
       res.send(result)
     })
-
+    // Save a Item data in db
+    app.post('/food', async (req, res) => {
+      const foodData = req.body;
+      const result = await foodsCollection.insertOne(foodData);
+      res.send(result);
+    })
+    // Get all Foods by specific user
+    app.get('/foods/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { buyer: email }
+      const result = await foodsCollection.find(query).toArray()
+      res.send(result)
+    })
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -65,6 +79,6 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Restaurant is open...')
+  res.send('Restaurant is open...')
 })
-app.listen(port, ()=> console.log(`server running on the port ${port}`))
+app.listen(port, () => console.log(`server running on the port ${port}`))
