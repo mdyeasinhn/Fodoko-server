cfe140b3937a7ce07f7142ec39a2f2c098c316a0
Md Yeasin <mdyeasinhn@gmail.com>
2024-07-11 14:49:52 +0600

    delete item for db

--- index.js
+++ index.js
@@ -66,7 +66,14 @@ async function run() {
       const result = await foodsCollection.find(query).toArray()
       res.send(result)
     })
-  
+    // Delete a item from db
+    app.delete('/food/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await foodsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
